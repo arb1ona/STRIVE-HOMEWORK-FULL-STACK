0a5c4891a54cfaa81e05d89ac43d5488022a4b35
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-08-25 23:30:37 +0200

    Basic Authentication

---
+++ MODULE 8/Day 2 - Basic-Authentication/src/server.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const express = require("express")
+const cors = require("cors")
+const { join } = require("path")
+const listEndpoints = require("express-list-endpoints")
+const mongoose = require("mongoose")
+
+const usersRouter = require("./services/users")
+
+const {
+  notFoundHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHandlers")
+
+const server = express()
+
+server.use(cors())
+const port = process.env.PORT
+
+const staticFolderPath = join(__dirname, "../public")
+server.use(express.static(staticFolderPath))
+server.use(express.json())
+
+server.use("/users", usersRouter)
+
+server.use(badRequestHandler)
+server.use(forbiddenHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+console.log(listEndpoints(server))
+
+mongoose
+  .connect("mongodb://localhost:27017/basic-auth", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Running on port", port)
+    })
+  )
+  .catch((err) => console.log(err))
